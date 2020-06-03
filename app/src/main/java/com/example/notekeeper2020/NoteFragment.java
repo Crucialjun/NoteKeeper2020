c50fae0566811a0710088ea4b0ad765a5f9f3b44
Crucial Jun <nich.otieno@gmail.com>
2020-06-03 12:52:43 +0300

    Populating the Spinner

--- app/src/main/java/com/example/notekeeper2020/NoteFragment.java
+++ app/src/main/java/com/example/notekeeper2020/NoteFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+
+import java.util.List;
+import java.util.Objects;
 
 public class NoteFragment extends Fragment {
 
@@ -25,5 +28,14 @@ public class NoteFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Spinner spinnerCourses = view.findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(Objects.requireNonNull(getActivity())
+                        ,android.R.layout.simple_spinner_item,courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCourses.setAdapter(adapterCourses);
     }
 }

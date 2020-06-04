d2ce433621f4e1df30fc37f4ce09bf77903a9e35
Crucialjun <nich.otieno@gmail.com>
2020-06-05 00:09:19 +0300

    Handle Arguments

--- app/src/main/java/com/example/notekeeper2020/NoteFragment.java
+++ app/src/main/java/com/example/notekeeper2020/NoteFragment.java
@@ -5,15 +5,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import java.util.List;
 import java.util.Objects;
 
 public class NoteFragment extends Fragment {
+
+    public static final String NOTE_INFO = "com.example.notekeeper2020.NOTE_INFO";
+    private NoteInfo mNote;
+    private Spinner mSpinnerCourses;
 
     @Override
     public View onCreateView(
@@ -21,21 +28,51 @@ public class NoteFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false);
+        View view =  inflater.inflate(R.layout.fragment_note, container, false);
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Spinner spinnerCourses = view.findViewById(R.id.spinner_courses);
+        if(getArguments() != null) {
+            NoteFragmentArgs args = NoteFragmentArgs.fromBundle(getArguments());
+            mNote = args.getNote();
+        }
+
+
+        mSpinnerCourses = view.findViewById(R.id.spinner_courses);
 
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
 
+        EditText textNoteTitle = view.findViewById(R.id.text_note_title);
+        EditText textNoteText = view.findViewById(R.id.text_note_text);
+
+
+        displayNotes(mSpinnerCourses,textNoteTitle,textNoteText);
+
         ArrayAdapter<CourseInfo> adapterCourses =
-                new ArrayAdapter<>(Objects.requireNonNull(getActivity())
+                new ArrayAdapter<>(requireActivity()
                         ,android.R.layout.simple_spinner_item,courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinnerCourses.setAdapter(adapterCourses);
+        mSpinnerCourses.setAdapter(adapterCourses);
+
+        getActivity().setTitle("Edit Note");
+
+
+
     }
+
+    private void displayNotes(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+            int courseIndex = courses.indexOf(mNote.getCourse());
+            spinnerCourses.setSelection(courseIndex);
+
+            textNoteTitle.setText(mNote.getTitle());
+            textNoteText.setText(mNote.getText());
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/notekeeper2020/NoteListFragment.java
@@ -0,0 +1,62 @@
+package com.example.notekeeper2020;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import java.util.List;
+import java.util.Objects;
+
+public class NoteListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeDisplayContent();
+        requireActivity().setTitle("Notes List");
+    }
+
+    private void initializeDisplayContent() {
+        final ListView listNotes = requireView().findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+
+        ArrayAdapter<NoteInfo> adapterNotes =
+                new ArrayAdapter<>(requireActivity(),android.R.layout.simple_list_item_1,notes);
+
+        listNotes.setAdapter(adapterNotes);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+
+                Fragment noteFragment = new NoteFragment();
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(NoteFragment.NOTE_INFO,note);
+                noteFragment.setArguments(bundle);
+
+                NoteListFragmentDirections.ActionNoteListFragmentToNoteFragment action = NoteListFragmentDirections.actionNoteListFragmentToNoteFragment(note);
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+    }
+}

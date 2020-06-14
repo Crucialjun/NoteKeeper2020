968b4f5461800f4219f3cce250576aacd6c0af61
Crucialjun <nich.otieno@gmail.com>
2020-06-14 14:01:53 +0300

    Navigation View Switch fragments

--- app/src/main/java/com/example/notekeeper2020/CoursesFragment.java
+++ app/src/main/java/com/example/notekeeper2020/CoursesFragment.java
@@ -9,21 +9,20 @@ import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.List;
 
-public class NoteListFragment extends Fragment {
+
+public class CoursesFragment extends Fragment {
 
     private ArrayAdapter<NoteInfo> mAdapterNotes;
-    private NoteRecyclerAdapter mNoteRecyclerAdapter;
+    private CourseRecyclerAdapter mCourseRecyclerAdapter;
     private RecyclerView mRecyclerItems;
-    private LinearLayoutManager mNotesLayoutManager;
+    private GridLayoutManager mCoursesLayoutManager;
 
     @Override
     public View onCreateView(
@@ -31,19 +30,9 @@ public class NoteListFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NoteListFragmentDirections.ActionNoteListFragmentToNoteFragment
-                        action = NoteListFragmentDirections.actionNoteListFragmentToNoteFragment();
-                Navigation.findNavController(v).navigate(action);
-            }
-        });
 
-        return view;
+        return inflater.inflate(R.layout.fragment_courses, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -51,7 +40,7 @@ public class NoteListFragment extends Fragment {
 
 
         initializeDisplayContent();
-        requireActivity().setTitle("Notes List");
+        requireActivity().setTitle("Courses");
     }
 
     private void initializeDisplayContent() {
@@ -76,28 +65,29 @@ public class NoteListFragment extends Fragment {
 //            }
 //        });
 
-        mRecyclerItems = requireView().findViewById(R.id.list_items);
-        mNotesLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerItems = requireView().findViewById(R.id.list_courses);
+        mCoursesLayoutManager = new GridLayoutManager(getActivity(),2);
 
 
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        mNoteRecyclerAdapter = new NoteRecyclerAdapter(getActivity(),notes);
-        displayNotes();
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        mCourseRecyclerAdapter = new CourseRecyclerAdapter(getActivity(),courses);
+        displayCourses();
 
     }
 
-    private void displayNotes() {
-        mRecyclerItems.setLayoutManager(mNotesLayoutManager);
-        mRecyclerItems.setAdapter(mNoteRecyclerAdapter);
+    private void displayCourses() {
+        mRecyclerItems.setLayoutManager(mCoursesLayoutManager);
+        mRecyclerItems.setAdapter(mCourseRecyclerAdapter);
 
         NavigationView navigationView = requireActivity().findViewById(R.id.nav_view);
         Menu menu = navigationView.getMenu();
-        menu.findItem(R.id.nav_notes).setChecked(true);
+        menu.findItem(R.id.nav_courses).setChecked(true);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mNoteRecyclerAdapter.notifyDataSetChanged();
+        mCourseRecyclerAdapter.notifyDataSetChanged();
     }
+
 }
--- app/src/main/java/com/example/notekeeper2020/MainActivity2.java
+++ app/src/main/java/com/example/notekeeper2020/MainActivity2.java
@@ -2,19 +2,28 @@ package com.example.notekeeper2020;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.notekeeper2020.ui.gallery.GalleryFragment;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private static final String TAG =  MainActivity2.class.getName();
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
@@ -24,17 +33,39 @@ public class MainActivity2 extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.noteListFragment)
+                R.id.noteListFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                Fragment fragment = new Fragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+                if(id==R.id.nav_notes){
+                    fragment = new NoteListFragment();
+
+                }else if(id==R.id.nav_courses){
+                    fragment =new CoursesFragment();
+                }
+
+                fragmentManager.beginTransaction().replace(R.id.nav_host_fragment,fragment).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -49,5 +80,11 @@ public class MainActivity2 extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    private void handleSelection(String message) {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view,message,Snackbar.LENGTH_LONG).show();
     }
 }
